5b0b7f23ba2657582328c2937e03dd8c06d14c26
renmu123 <1101022351@qq.com>
2018-05-17 00:00:38 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+SENDER = 'renmu12345678@163.com'  # 寄信人邮箱
+PASSWORD = 'fyx19961017'  # 寄信人密码
+FROM = 'XX'  # 寄信人昵称
+SUBJECT = 'yyy'  # 主题
+SUCCESS_MSG = 'success'  # 发送成功提示
+
---
+++ cp22.py
@@ -0,0 +1,21 @@
+import requests
+from mail import send_mail
+
+url = 'http://www.allcpp.cn/allcpp/ticket/getTicketTypeList.do'
+data = {
+    'eventmainid': 450
+}
+headers = {
+    'Host': 'www.allcpp.cn',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+    'Origin': 'http://www.dianping.com',
+    'Referer': 'http://www.allcpp.cn/m/e/450/ticket.do',
+    'Cookie': 'JALKSJFJASKDFJKALSJDFLJSF=183122104165daf6d371580142f7b1f56a76ee3de0d1223.167.62.204_633965124; JSESSIONID=62BD4B0BF9CF566CA3A9B4A7EADA11CD; token=43971884377E14018B997B6E1D68B491F94562BE594902C018D5EF13DB98C7FC201246381685EC74E9DA49E909D70E3993D20802E4DE09A74B320C73A2A0D4F3; Hm_lvt_75e110b2a3c6890a57de45bd2882ec7c=1526484787; Hm_lpvt_75e110b2a3c6890a57de45bd2882ec7c=1526484787'
+}
+r = requests.post(url, headers=headers, data=data)
+
+value = []
+for item in r.json()['ticketTypeList']:
+    value.append(item['ticketTypeName'] + str(item['remainCount']))
+
+status = send_mail('\n'.join(value), '1101022351@qq.com')
